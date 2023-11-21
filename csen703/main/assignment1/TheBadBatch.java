f07c5aec9c4365c1f444c84af993da859ee5daf8
saradiaa9 <127624812+saradiaa9@users.noreply.github.com>
2023-11-21 20:41:56 +0200

    Create TheBadBatch.java

---
+++ csen703/main/assignment1/TheBadBatch.java
@@ -0,0 +1,30 @@
+package csen703.main.assignment1;
+
+/**
+ * Write your info here
+ * 
+ * @Team : 4
+ * @name : Sarah Diaaeldeen Hussain, Layla Abougabal, Kareem Osama.
+ * @id : 52-1678, 52-9702, 52-10614.
+ * @TutorialNumber : T11, T11, T11.
+ */
+
+
+public class TheBadBatch {
+	
+	public static boolean TatooineToNabooDivideAndConquer(int [] fuel) {
+		return false;
+	}
+	
+	public static boolean TatooineToNabooGreedy(int [] fuel) {
+		return false;
+	}
+
+	public static void main(String[] args) {
+		
+		int [] fuel={2,3,1,1,4};
+		System.out.println(TatooineToNabooDivideAndConquer(fuel));
+		System.out.println(TatooineToNabooGreedy(fuel));
+	}
+
+}
